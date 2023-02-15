81bd0917cd76cb13bdd6881038cebeff47e99fbf
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-02-15 12:22:34 +0300

    set up dispatcher servlet initializer

--- src/main/java/me/mikholsky/configuration/AppAnnotationConfigDispatcherServletInitializer.java
+++ src/main/java/me/mikholsky/configuration/AppAnnotationConfigDispatcherServletInitializer.java
@@ -1,2 +1,20 @@
-package me.mikholsky.configuration;public class AppAnnotationConfigDispatcherServletInitializer {
+package me.mikholsky.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppAnnotationConfigDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[]{};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[]{AppWebMvcConfigurer.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
 }
